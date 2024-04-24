299bb4422060c6210cd39a4ae66bdb5f3abd09f6
Jonathan Kwan <jonathankwan056@gmail.com>
2024-04-24 12:36:24 +0800

    feat: tugas pertemuan 5

---
+++ H071221067/Pertemuan_5/app/src/main/java/com/example/tuprak_5/DataSource.java
@@ -0,0 +1,102 @@
+package com.example.tuprak_5;
+
+import java.util.ArrayList;
+
+public class DataSource {
+
+    public static ArrayList<Instagram> instagrams = generateDummyInstagrams();
+
+    private static ArrayList<Instagram> generateDummyInstagrams() {
+        ArrayList<Instagram> instagrams1 = new ArrayList<>();
+        instagrams1.add(new Instagram("genshinimpact","Genshin Impact"
+                ,"The \"Mondstadt Tourism Revitalization Project\"... Has Gone Bust!\n" +
+                "\n" +
+                "\"*sigh*... Looks like I completely underestimated how much this project would end up costing... I don't even have enough to get it off the ground! But as long as I pull out before it's too late, there's still a chance I'll be able to recover my losses and try again some day... Please, take this, and support me next time too!\"\n" +
+                "\n" +
+                "Claim Reward (Primogems ×40): https://hoyo.link/bhAiFHAL\n" +
+                "\n" +
+                "*Rewards can be claimed before April 3, 2024 23:59 (UTC+8). Please head to the web page to claim your reward as soon as possible.\n" +
+                "After claiming your reward on the web page, it will be sent to your in-game mailbox. The mail will expire after 30 days, so don't forget to claim your reward in time.\n" +
+                "\n" +
+                "#MondstadtTourism #GenshinImpact"
+                ,R.drawable.genshin_avatar,R.drawable.genshin_post));
+
+        instagrams1.add(new Instagram("samsungindonesia", "Samsung Indonesia"
+                ,"Asah kreativitas dan imajinasi si Kecil selama bulan Ramadan dengan #GalaxyTabA9 \uD83D\uDE80✨\n" +
+                "\n" +
+                "Ajak si Kecil bermain & belajar dengan berbagai aplikasi hiburan dan edukasi yang ada di Samsung Kids. Dapatkan promo #MakinManTAB cashback sampai Rp 200.000, klik link di bio!"
+                ,R.drawable.samsung_avatar,R.drawable.samsung_post));
+
+        instagrams1.add(new Instagram("akanecco_2323", "黒川あかね"
+                ,"私頑張ったから、きっと来てくれるよね。。。\n"
+                ,R.drawable.akane_avatar, R.drawable.akane_post));
+
+        instagrams1.add(new Instagram("realmobilelegendsid", "Mobile Legends: Bang Bang (ID)"
+                ,"Orang yang komen di postingan ini dapat 10000 diamond!\n" +
+                ".\n" +
+                ".\n" +
+                ".\n" +
+                ".\n" +
+                ".\n" +
+                "Tapi boong \uD83D\uDE0B yang ga boong cuma StarLight April Lylia \"\"Magitech Arsenal\"\" udah rilis yaa! #AprilMop #mlbb #mlbbid #mobilelegends #mobilelegendsid #StarLightLylia #Lylia"
+                ,R.drawable.mlbb_avatar,R.drawable.mlbb_post));
+
+        instagrams1.add(new Instagram("mpl.id.official","MPL Indonesia"
+                ,"Ini dia Klasemen sementara kita di akhir Minggu ke-4! \uD83D\uDD25\n" +
+                "\n" +
+                "Berbagai match seru, dramatis, dan penuh sensasi sudah tersaji selama minggu ke-4 ini, dan inilah hasil klasemen sementara kita! ‼\uFE0F\n" +
+                "\n" +
+                "Tim favorit kalian ada di posisi ke berapa nih guys? Komen di bawah! \uD83E\uDD29\n" +
+                "\n" +
+                "#WeOwnThis #MPLIDS13"
+                ,R.drawable.mpl_avatar, R.drawable.mpl_post));
+
+        instagrams1.add(new Instagram("wuthering_waves","Wuthering Waves"
+                , "Open-world Action RPG Wuthering Waves is set to release on 2024/05/22 (PT) on PC, iOS, Android, and Epic Games Store. Pre-registration is now open on various platforms!\n" +
+                "Pre-register now: https://akioversea.onelink.me/lKVo/7n76bkvu\n" +
+                "\n" +
+                "✦WAKING OF A WORLD✦\n" +
+                "\n" +
+                "#WutheringWaves\n" +
+                "#WutheringWavesLaunch"
+                ,R.drawable.ww_avatar,R.drawable.ww_post));
+
+        instagrams1.add(new Instagram("honkaistarrail","Honkai: Star Rail Official"
+                ,"Exclusive embargo lifted! First look at super-secret analysis｜Pom-Pom\n" +
+                "\n" +
+                "To help everyone more quickly get into the spirit (they'd better be) of the movie, we've specially prepared this exclusive and classified Pom-Pom (Destruction: Physical) analysis!\n" +
+                "\n" +
+                "#StarRailSFXFilm\n" +
+                "#PomPomAnglesfortheOscar\n" +
+                "#PomPomPower\n" +
+                "#AprilFoolsHSR"
+                ,R.drawable.hsr_avatar, R.drawable.hsr_post));
+
+        instagrams1.add(new Instagram("lalalalisa_m", "LISA"
+                ,"A special night with @bulgari & first public look with my 2nd watch collab \uD83D\uDDA4\n" +
+                "\n" +
+                "#불가리 #BulgariStudio #Bzero1"
+                ,R.drawable.lisa_avatar, R.drawable.lisa_post));
+
+        instagrams1.add(new Instagram("oppoindonesia", "OPPO Indonesia"
+                ,"Siapa yang sudah bersiap untuk menyambut hari Raya? Komen apa saja yang sudah kamu siapkan! \uD83D\uDC47✍\uFE0F\n" +
+                "\n" +
+                "Raih #OPPOBerkahRamadan sekarang bareng #OPPOReno11F \uD83D\uDED2\n" +
+                "\n" +
+                "#ThePortraitExpert #FantasticReno"
+                ,R.drawable.oppo_avatar, R.drawable.oppo_post));
+
+        instagrams1.add(new Instagram("gamerwk_id", "Gamewrk"
+                ,"\uD835\uDDEA\uD835\uDE02\uD835\uDE01\uD835\uDDF5\uD835\uDDF2\uD835\uDDFF\uD835\uDDF6\uD835\uDDFB\uD835\uDDF4 \uD835\uDDEA\uD835\uDDEE\uD835\uDE03\uD835\uDDF2\uD835\uDE00 \uD835\uDDE3\uD835\uDDEE\uD835\uDE00\uD835\uDE01\uD835\uDDF6\uD835\uDDF8\uD835\uDDEE\uD835\uDDFB \uD835\uDDE5\uD835\uDDF6\uD835\uDDF9\uD835\uDDF6\uD835\uDE00 \uD835\uDFEE\uD835\uDFEF \uD835\uDDE0\uD835\uDDF2\uD835\uDDF6 \uD835\uDE02\uD835\uDDFB\uD835\uDE01\uD835\uDE02\uD835\uDDF8 \uD835\uDDE0\uD835\uDDFC\uD835\uDDEF\uD835\uDDF6\uD835\uDDF9\uD835\uDDF2 \uD835\uDDF1\uD835\uDDEE\uD835\uDDFB \uD835\uDDE3\uD835\uDDD6\n" +
+                "\n" +
+                "Kuro Games akhirnya memberi konfirmasi resmi untuk jadwal rilis Wuthering Waves yang ternyata memang jatuh pada 23 Mei mendatang.\n" +
+                "\n" +
+                "Melalui sesi Special Broadcast beberapa saat lalu, mereka ikut membagikan beragam info tambahan termasuk ketersediaan gamenya secara serempak untuk platform Android, iOS, PC, dan Epic Games Store.\n" +
+                "\n" +
+                "Bagi yang mengharapkan adanya versi PlayStation untuk sementara ini belum ada konfirmasi apapun. Tapi jika mereka memang berencana untuk membawa gamenya ke PS5, maka rilisnya baru akan menyusul pada jadwal terpisah di masa depan.\n" +
+                "\n" +
+                "#WutheringWaves #KuroGames"
+                ,R.drawable.gamerwk_avatar, R.drawable.gamerwk_post));
+        return instagrams1;
+    }
+}
